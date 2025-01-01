f9e11cc41cf7bd3d3ca187b8dd156b988efb7496
Miguel Angel Villegas <chumilo227@gmail.com>
2025-01-01 17:19:08 -0500

    add project routes to server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 import { connectDB } from './config/db';
+import { router } from './Routes/projectRoutes';
 
 connectDB();
 
 const app = express();
+app.use('/api/projects', router);
 
 export default app;
