99ae6a766335cc315da7eccd5f83f976ab0bf6e8
Miguel Angel Villegas <chumilo227@gmail.com>
2025-01-01 18:57:06 -0500

    add ProjectController with create and getAllProjects methods

---
+++ server/src/controllers/ProjectController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import Project from '../models/Project';
+
+export class ProjectController {
+	static createProject = async (req: Request, res: Response) => {
+		const project = new Project(req.body);
+
+		try {
+			await project.save();
+			res.send('Projecto creado exitosamente');
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	static getAllProjects = async (req: Request, res: Response) => {
+		res.send('Todos los registros desde mongoDB');
+	};
+}
