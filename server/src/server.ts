3eb434da80b7dff63b31753e91f7a3dd482f860c
Miguel Angel Villegas <chumilo227@gmail.com>
2025-01-01 17:31:53 -0500

    add JSON body parsing middleware to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,9 @@ import { router } from './Routes/projectRoutes';
 connectDB();
 
 const app = express();
+app.use(express.json());
+
+// Routes
 app.use('/api/projects', router);
 
 export default app;
