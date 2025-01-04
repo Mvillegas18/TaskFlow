061a181e81c087f903e70ac8058883b90d8cdb2f
Miguel Angel Villegas <chumilo227@gmail.com>
2025-01-03 20:41:09 -0500

    add status field to ITask interface and update task schema with status enum

--- server/src/models/Task.ts
+++ server/src/models/Task.ts
@@ -1,9 +1,20 @@
 import { Document, model, Schema, Types } from 'mongoose';
 
+const taskStatus = {
+	PENDING: 'pending',
+	ON_HOLD: 'onHold',
+	IN_PROGRESS: 'inProgress',
+	UNDER_REVIEW: 'underReview',
+	COMPLETED: 'completed',
+} as const;
+
+export type TaskType = (typeof taskStatus)[keyof typeof taskStatus];
+
 export interface ITask extends Document {
 	name: string;
 	description: string;
 	project: Types.ObjectId;
+	status: TaskType;
 }
 
 export const taskSchema: Schema = new Schema(
@@ -21,6 +32,11 @@ export const taskSchema: Schema = new Schema(
 		project: {
 			type: Types.ObjectId,
 			ref: 'Project',
+		},
+		status: {
+			type: String,
+			enum: Object.values(taskStatus),
+			default: taskStatus.PENDING,
 		},
 	},
 	{ timestamps: true }
