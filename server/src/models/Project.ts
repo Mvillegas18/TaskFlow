6e018c3f582e9bc32c3433220a0320a64b9519bd
Miguel Angel Villegas <chumilo227@gmail.com>
2025-01-03 20:32:40 -0500

    add tasks field to IProject interface and update project schema to include task references

--- server/src/models/Project.ts
+++ server/src/models/Project.ts
@@ -1,28 +1,39 @@
-import { Document, model, Schema } from 'mongoose';
+import { Document, model, PopulatedDoc, Schema, Types } from 'mongoose';
+import type { ITask } from './Task';
 
 export interface IProject extends Document {
 	projectName: string;
 	clientName: string;
 	description: string;
+	tasks: PopulatedDoc<ITask & Document>[];
 }
 
-const projectSchema: Schema = new Schema({
-	projectName: {
-		type: String,
-		require: true,
-		trim: true,
+const projectSchema: Schema = new Schema(
+	{
+		projectName: {
+			type: String,
+			require: true,
+			trim: true,
+		},
+		clientName: {
+			type: String,
+			require: true,
+			trim: true,
+		},
+		description: {
+			type: String,
+			require: true,
+			trim: true,
+		},
+		tasks: [
+			{
+				type: Types.ObjectId,
+				ref: 'Task',
+			},
+		],
 	},
-	clientName: {
-		type: String,
-		require: true,
-		trim: true,
-	},
-	description: {
-		type: String,
-		require: true,
-		trim: true,
-	},
-});
+	{ timestamps: true }
+);
 
 const Project = model<IProject>('Project', projectSchema);
 export default Project;
