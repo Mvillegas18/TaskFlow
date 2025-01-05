4895dadac54a18883288adf4d6f36ef433e91727
Miguel Angel Villegas <chumilo227@gmail.com>
2025-01-04 21:26:03 -0500

    add middleware to validate project existence in requests

---
+++ server/src/middleware/project.ts
@@ -0,0 +1,32 @@
+import type { NextFunction, Request, Response } from 'express';
+import Project, { IProject } from '../models/Project';
+
+declare global {
+	namespace Express {
+		interface Request {
+			project: IProject;
+		}
+	}
+}
+
+export const validateProjectExist = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const { projectId } = req.params;
+		const project = await Project.findById(projectId);
+
+		if (project) {
+			req.project = project;
+			next();
+		} else {
+			res
+				.status(404)
+				.json({ error: new Error('No existe el proyecto').message });
+		}
+	} catch (error) {
+		res.status(505).json({ error: 'Hubo un error' });
+	}
+};
