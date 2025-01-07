f1d01e6502c1f1d0542b597522de8cf6549f35a6
Miguel Angel Villegas <chumilo227@gmail.com>
2025-01-06 19:57:10 -0500

    add updateTask and deleteTask methods to TaskController for task management

--- server/src/controllers/TaskController.ts
+++ server/src/controllers/TaskController.ts
@@ -43,4 +43,46 @@ export class TaskController {
 			res.status(500).json({ error });
 		}
 	};
+
+	static updateTask = async (req: Request, res: Response) => {
+		try {
+			const { taskId } = req.params;
+			const task = await Task.findById(taskId);
+			if (!task) {
+				res
+					.status(404)
+					.json({ error: new Error('Tarea no encontrada').message });
+			}
+			if (task?.project.toString() !== req.project.id) {
+				res.status(400).json({ error: new Error('Accion no valida').message });
+			}
+			if (task) {
+				task.name = req.body.name;
+				task.description = req.body.description;
+			}
+			res.send('Tarea actualizada correctamente');
+		} catch (error) {
+			res.status(500).json({ error });
+		}
+	};
+
+	static deleteTask = async (req: Request, res: Response) => {
+		try {
+			const { taskId } = req.params;
+			const task = await Task.findById(taskId, req.body);
+			if (!task) {
+				res
+					.status(404)
+					.json({ error: new Error('Tarea no encontrada').message });
+			}
+
+			req.project.tasks = req.project.tasks.filter(
+				(task) => task?.toString() !== taskId
+			);
+			await Promise.allSettled([Task.deleteOne(), req.project.save()]);
+			res.send('Tarea eliminada correctamente');
+		} catch (error) {
+			res.status(500).json({ error });
+		}
+	};
 }
