1089ae279d21da8c05807cdecdb0498d375b86fd
Miguel Angel Villegas <chumilo227@gmail.com>
2025-01-06 20:33:23 -0500

    feat: add taskBelongToProject middleware to project routes for task ownership validation

--- server/src/Routes/projectRoutes.ts
+++ server/src/Routes/projectRoutes.ts
@@ -3,7 +3,7 @@ import { body, param } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
 import { TaskController } from '../controllers/TaskController';
 import { projectExist } from '../middleware/project';
-import { taskExist } from '../middleware/task';
+import { taskBelongToProject, taskExist } from '../middleware/task';
 import { handleInputErrors } from '../middleware/validation';
 
 export const router = Router();
@@ -60,6 +60,7 @@ router.delete(
 
 // Routes for tasks
 router.param('projectId', projectExist);
+router.param('projectId', taskBelongToProject);
 
 router.post(
 	'/:projectId/task',
